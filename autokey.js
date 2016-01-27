c844b93defc8942c89e37a05a7ff2ccfd7025005
Allen Luce <aluce@adonnetwork.com>
2016-01-27 09:58:21 -0800

    Standardify code style.

--- autokey.js
+++ autokey.js
@@ -1,53 +1,59 @@
-function Autokey(key, alphabet) {
-  this.key = [];
-  for (var i = 0, ii = key.length; i < ii; ++i)
-    this.key[i] = key[i].charCodeAt(0);
-  this.betaalph = [];
-  for (var i = 0, ii = alphabet.length; i < ii; ++i) 
-    this.betaalph[alphabet[i]] = i;
-  this.alphabet = alphabet;
-  this.keylen = key.length;
-  this.alphalen = alphabet.length;
+function Autokey (key, alphabet) {
+  this.key = []
+  for (var i = 0, ii = key.length; i < ii; ++i) {
+    this.key[i] = key[i].charCodeAt(0)
+  }
+  this.betaalph = []
+  for (i = 0, ii = alphabet.length; i < ii; ++i) {
+    this.betaalph[alphabet[i]] = i
+  }
+  this.alphabet = alphabet
+  this.keylen = key.length
+  this.alphalen = alphabet.length
 }
 
-Autokey.prototype.encode = function(raw) {
-  var res = "";
-  var cc = this.keylen;
-  var last = [];
-  var k;
-  for (var i = 0, ii = raw.length; i < ii; ++i)
-    if (this.betaalph[raw[i]] == undefined)
+Autokey.prototype.encode = function (raw) {
+  var res = ''
+  var cc = this.keylen
+  var last = []
+  var k
+  for (var i = 0, ii = raw.length; i < ii; ++i) {
+    if (this.betaalph[raw[i]] === undefined) {
       res += raw[i]
-    else {
-      if (cc-- > 0)
+    } else {
+      if (cc-- > 0) {
         k = this.key[cc]
-      else
+      } else {
         k = this.betaalph[last.shift()]
+      }
 
-      var c = this.alphabet[(this.betaalph[raw[i]] + k) % this.alphalen];
-      res += c;
-      last.push(c);
+      var c = this.alphabet[(this.betaalph[raw[i]] + k) % this.alphalen]
+      res += c
+      last.push(c)
     }
-  return res;
+  }
+  return res
 }
 
-Autokey.prototype.decode = function(raw) {
-  var res = "";
-  var cc = this.keylen;
-  var last = [];
-  var k;
-  for (var i = 0, ii = raw.length; i < ii; ++i)
-    if (this.betaalph[raw[i]] == undefined)
+Autokey.prototype.decode = function (raw) {
+  var res = ''
+  var cc = this.keylen
+  var last = []
+  var k
+  for (var i = 0, ii = raw.length; i < ii; ++i) {
+    if (this.betaalph[raw[i]] === undefined) {
       res += raw[i]
-    else {
-      if (cc-- > 0)
+    } else {
+      if (cc-- > 0) {
         k = this.key[cc] % this.alphalen
-      else
+      } else {
         k = this.betaalph[last.shift()]
-      res += this.alphabet[(this.betaalph[raw[i]] - k + this.alphalen) % this.alphalen];
-      last.push(raw[i]);
+      }
+      res += this.alphabet[(this.betaalph[raw[i]] - k + this.alphalen) % this.alphalen]
+      last.push(raw[i])
     }
-  return res;
+  }
+  return res
 }
 
-module.exports = Autokey;
+module.exports = Autokey
--- test/test-autokey.js
+++ test/test-autokey.js
@@ -1,47 +1,45 @@
-var expect = require('chai').expect;
-var autokey = require('../autokey');
+/* global describe, before, it */
+var expect = require('chai').expect
+var Autokey = require('../autokey')
 
-var shuffle = function(str) {
-  return str.split('').sort(function(){return 0.5-Math.random()}).join('');
-};
+var shuffle = function (str) {
+  return str.split('').sort(function () { return 0.5 - Math.random() }).join('')
+}
 
-describe("Autokey", function() {
-  before(function() {
-    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var key = "SODFBOIBOEIBOISBDOBLBUVLIUBIEBEHFWF";
-    this.cipher = new autokey(key, alphabet);
-  });
+describe('Autokey', function () {
+  before(function () {
+    var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var key = 'SODFBOIBOEIBOISBDOBLBUVLIUBIEBEHFWF'
+    this.cipher = new Autokey(key, alphabet)
+  })
 
-  it("encodes properly", function() {
-    expect(this.cipher.encode("WELLDONEISBETTERTHANWELLSAID")).to.equal("ONDFUCEZWZWCBASPHIQBBZMZNRJR");
-  });
+  it('encodes properly', function () {
+    expect(this.cipher.encode('WELLDONEISBETTERTHANWELLSAID')).to.equal('ONDFUCEZWZWCBASPHIQBBZMZNRJR')
+  })
 
-  it("decodes properly", function() {
-    expect(this.cipher.decode("ONDFUCEZWZWCBASPHIQBBZMZNRJR")).to.equal("WELLDONEISBETTERTHANWELLSAID");
-  });
-     
-  it("is reversible", function() {
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789" ;
-    var cipher = new autokey("somekey here", possible);
-    var str = shuffle(possible);
-    expect(cipher.decode(cipher.encode(str))).to.equal(str);
-  });
-  
-  it("doesn't touch characters not in the plaintext alphabet", function() {
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    var cipher = new autokey("some other key her", possible);
-    var str = "This is my text!"
-    expect(cipher.encode(str)).to.match(/[A-Za-z0-9]{4} [A-Za-z0-9]{2} [A-Za-z0-9]{2} [A-Za-z0-9]{4}\!/);
-    expect(cipher.decode(cipher.encode(str))).to.equal(str);
-  });
+  it('decodes properly', function () {
+    expect(this.cipher.decode('ONDFUCEZWZWCBASPHIQBBZMZNRJR')).to.equal('WELLDONEISBETTERTHANWELLSAID')
+  })
 
-  it("Works with lots of non-alphabet in it", function() {
-    var possible = "abcdefghijklmnopqrstuvwxyz";
-    var cipher = new autokey("moy", possible);
-    var str = "This has a lot of non-alphabet!"
-    expect(cipher.decode(cipher.encode(str))).to.equal(str);
-  });
+  it('is reversible', function () {
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var cipher = new Autokey('somekey here', possible)
+    var str = shuffle(possible)
+    expect(cipher.decode(cipher.encode(str))).to.equal(str)
+  })
 
-  
-});
+  it("doesn't touch characters not in the plaintext alphabet", function () {
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    var cipher = new Autokey('some other key her', possible)
+    var str = 'This is my text!'
+    expect(cipher.encode(str)).to.match(/[A-Za-z0-9]{4} [A-Za-z0-9]{2} [A-Za-z0-9]{2} [A-Za-z0-9]{4}\!/)
+    expect(cipher.decode(cipher.encode(str))).to.equal(str)
+  })
 
+  it('Works with lots of non-alphabet in it', function () {
+    var possible = 'abcdefghijklmnopqrstuvwxyz'
+    var cipher = new Autokey('moy', possible)
+    var str = 'This has a lot of non-alphabet!'
+    expect(cipher.decode(cipher.encode(str))).to.equal(str)
+  })
+})
